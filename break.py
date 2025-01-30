1a2ec66b7e831a4faaa22aecf81043f279a0b7a3
Sadab Masrur <sadabmasrur08@gmail.com>
2025-01-30 22:08:45 +0600

    first commit

---
+++ break.py
@@ -0,0 +1,11 @@
+a=input("Enter a word: ")
+
+for i in a:
+    
+    if i=="A" or i=="a":
+        
+        print("A is found")
+        break
+    
+    else:
+        print(f"A not found for [{i}]")
